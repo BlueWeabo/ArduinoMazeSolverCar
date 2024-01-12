d2417a32999650ffa3a62d78f2c3c3453c78f2e0
BlueWeabo <ilia.iliev2005@gmail.com>
2024-01-12 15:29:29 +0200

    redo part of code for a single motor controller

--- MazeSolver/MazeSolver.ino
+++ MazeSolver/MazeSolver.ino
@@ -1,30 +1,27 @@
 
-#define LEFT 2
-#define FRONT 3
-#define MOTOR_FRONT_L_F 4
-#define MOTOR_FRONT_L_B 5
-#define MOTOR_FRONT_R_F 7
-#define MOTOR_FRONT_R_B 6
-#define MOTOR_BACK_L_F 8
-#define MOTOR_BACK_L_B 9
-#define MOTOR_BACK_R_F 13
-#define MOTOR_BACK_R_B 12
+#define LEFT_T 2
+#define LEFT_E 3
+#define FRONT_T 8
+#define FRONT_E 9
+#define MOTOR_L_F 4
+#define MOTOR_L_B 5
+#define MOTOR_R_F 7
+#define MOTOR_R_B 6
 #define MOTOR_SPEED_L 10
 #define MOTOR_SPEED_R 11
 #define SENSOR_CALC 340 / 1000 / 2
 
 void setup() {
-  pinMode(MOTOR_FRONT_L_F, OUTPUT);
-  pinMode(MOTOR_FRONT_L_B, OUTPUT);
-  pinMode(MOTOR_FRONT_R_F, OUTPUT);
-  pinMode(MOTOR_FRONT_R_B, OUTPUT);
-  pinMode(MOTOR_BACK_L_F, OUTPUT);
-  pinMode(MOTOR_BACK_L_B, OUTPUT);
-  pinMode(MOTOR_BACK_R_F, OUTPUT);
-  pinMode(MOTOR_BACK_R_B, OUTPUT);
+  pinMode(MOTOR_L_F, OUTPUT);
+  pinMode(MOTOR_L_B, OUTPUT);
+  pinMode(MOTOR_R_F, OUTPUT);
+  pinMode(MOTOR_R_B, OUTPUT);
   pinMode(MOTOR_SPEED_L, OUTPUT);
   pinMode(MOTOR_SPEED_R, OUTPUT);
-  // random comment
+  pinMode(LEFT_T, OUTPUT);
+  pinMode(LEFT_E, INPUT);
+  pinMode(FRONT_T, OUTPUT);
+  pinMode(FRONT_E, INPUT);
 }
 
 void loop() {
@@ -42,75 +39,55 @@ void loop() {
 }
 
 float checkLeftSensor() {
-  pinMode(LEFT, OUTPUT);
-  digitalWrite(LEFT, LOW);
+  digitalWrite(LEFT_T, LOW);
   delayMicroseconds(2);
-  digitalWrite(LEFT, HIGH);
+  digitalWrite(LEFT_T, HIGH);
   delayMicroseconds(10);
-  digitalWrite(LEFT, LOW);
-  pinMode(LEFT, INPUT);
-  return float(pulseIn(LEFT, HIGH)) * SENSOR_CALC;
+  digitalWrite(LEFT_T, LOW);
+  return float(pulseIn(LEFT_E, HIGH)) * SENSOR_CALC;
 }
 
 float checkFrontSensor() {
-  pinMode(FRONT, OUTPUT);
-  digitalWrite(FRONT, LOW);
+  digitalWrite(FRONT_T, LOW);
   delayMicroseconds(2);
-  digitalWrite(FRONT, HIGH);
+  digitalWrite(FRONT_T, HIGH);
   delayMicroseconds(10);
-  digitalWrite(FRONT, LOW);
-  pinMode(FRONT, INPUT);
-  return float(pulseIn(FRONT, HIGH)) * SENSOR_CALC;
+  digitalWrite(FRONT_T, LOW);
+  return float(pulseIn(FRONT_E, HIGH)) * SENSOR_CALC;
 }
 
 void goForwards() {
-  digitalWrite(MOTOR_FRONT_L_F, HIGH);
-  digitalWrite(MOTOR_FRONT_L_B, LOW);
-  digitalWrite(MOTOR_FRONT_R_F, HIGH);
-  digitalWrite(MOTOR_FRONT_R_B, LOW);
-  digitalWrite(MOTOR_BACK_L_F, HIGH);
-  digitalWrite(MOTOR_BACK_L_B, LOW);
-  digitalWrite(MOTOR_BACK_R_F, HIGH);
-  digitalWrite(MOTOR_BACK_R_B, LOW);
+  digitalWrite(MOTOR_L_F, HIGH);
+  digitalWrite(MOTOR_L_B, LOW);
+  digitalWrite(MOTOR_R_F, HIGH);
+  digitalWrite(MOTOR_R_B, LOW);
   analogWrite(MOTOR_SPEED_L, 180);
   analogWrite(MOTOR_SPEED_R, 180);
 }
 
 void goBackwards() {
-  digitalWrite(MOTOR_FRONT_L_F, LOW);
-  digitalWrite(MOTOR_FRONT_L_B, HIGH);
-  digitalWrite(MOTOR_FRONT_R_F, LOW);
-  digitalWrite(MOTOR_FRONT_R_B, HIGH);
-  digitalWrite(MOTOR_BACK_L_F, LOW);
-  digitalWrite(MOTOR_BACK_L_B, HIGH);
-  digitalWrite(MOTOR_BACK_R_F, LOW);
-  digitalWrite(MOTOR_BACK_R_B, HIGH);
+  digitalWrite(MOTOR_L_F, LOW);
+  digitalWrite(MOTOR_L_B, HIGH);
+  digitalWrite(MOTOR_R_F, LOW);
+  digitalWrite(MOTOR_R_B, HIGH);
   analogWrite(MOTOR_SPEED_L, 180);
   analogWrite(MOTOR_SPEED_R, 180);
 }
 
 void goLeft() {
-  digitalWrite(MOTOR_FRONT_L_F, HIGH);
-  digitalWrite(MOTOR_FRONT_L_B, LOW);
-  digitalWrite(MOTOR_FRONT_R_F, HIGH);
-  digitalWrite(MOTOR_FRONT_R_B, LOW);
-  digitalWrite(MOTOR_BACK_L_F, HIGH);
-  digitalWrite(MOTOR_BACK_L_B, LOW);
-  digitalWrite(MOTOR_BACK_R_F, HIGH);
-  digitalWrite(MOTOR_BACK_R_B, LOW);
+  digitalWrite(MOTOR_L_F, HIGH);
+  digitalWrite(MOTOR_L_B, LOW);
+  digitalWrite(MOTOR_R_F, HIGH);
+  digitalWrite(MOTOR_R_B, LOW);
   analogWrite(MOTOR_SPEED_L, 100);
   analogWrite(MOTOR_SPEED_R, 230);
 }
 
 void goRight() {
-  digitalWrite(MOTOR_FRONT_L_F, LOW);
-  digitalWrite(MOTOR_FRONT_L_B, HIGH);
-  digitalWrite(MOTOR_FRONT_R_F, HIGH);
-  digitalWrite(MOTOR_FRONT_R_B, LOW);
-  digitalWrite(MOTOR_BACK_L_F, LOW);
-  digitalWrite(MOTOR_BACK_L_B, HIGH);
-  digitalWrite(MOTOR_BACK_R_F, HIGH);
-  digitalWrite(MOTOR_BACK_R_B, LOW);
+  digitalWrite(MOTOR_L_F, HIGH);
+  digitalWrite(MOTOR_L_B, LOW);
+  digitalWrite(MOTOR_R_F, LOW);
+  digitalWrite(MOTOR_R_B, HIGH);
   analogWrite(MOTOR_SPEED_L, 180);
   analogWrite(MOTOR_SPEED_R, 180);
 }
