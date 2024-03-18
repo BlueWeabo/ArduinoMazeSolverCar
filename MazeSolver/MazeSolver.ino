d345fa4724470f645c895008de0c62b2314da855
BlueWeabo <ilia.iliev2005@gmail.com>
2024-03-18 18:35:46 +0200

    almost working

--- MazeSolver/MazeSolver.ino
+++ MazeSolver/MazeSolver.ino
@@ -1,17 +1,19 @@
 
-#define LEFT_T 2
-#define LEFT_E 3
-#define FRONT_T 8
-#define FRONT_E 9
-#define MOTOR_L_F 4
-#define MOTOR_L_B 5
-#define MOTOR_R_F 7
-#define MOTOR_R_B 6
+#define LEFT_T 3
+#define LEFT_E 4
+#define FRONT_T 11
+#define FRONT_E 12
+#define MOTOR_L_F 8
+#define MOTOR_L_B 7
+#define MOTOR_R_F 6
+#define MOTOR_R_B 5
 #define MOTOR_SPEED_L 10
-#define MOTOR_SPEED_R 11
-#define SENSOR_CALC 340 / 1000 / 2
+#define MOTOR_SPEED_R 9
+#define SPEED 50
+const float SENSOR_CALC = 29.0 * 2.0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(MOTOR_L_F, OUTPUT);
   pinMode(MOTOR_L_B, OUTPUT);
   pinMode(MOTOR_R_F, OUTPUT);
@@ -27,12 +29,19 @@ void setup() {
 void loop() {
   float leftSensor = checkLeftSensor();
   float frontSensor = checkFrontSensor();
-  if (leftSensor > 3.6f) {
+  Serial.println(leftSensor);
+  Serial.println(frontSensor);
+  if (leftSensor > 5.8f) {
     goLeft();
-  } else if (frontSensor < 4.5f) {
-    goRight();
-  } else if (frontSensor < 2.5f) {
+  } else if (frontSensor > 200.00f) {
     goBackwards();
+  } else if (frontSensor < 3.00f) {
+    goBackwards();
+  } else if (frontSensor < 4.50f) {
+    goRight();
+    delay(5 00);
+  } else if (leftSensor < 3.00f) {
+    goSmallRight();
   } else {
     goForwards();
   }
@@ -44,7 +53,7 @@ float checkLeftSensor() {
   digitalWrite(LEFT_T, HIGH);
   delayMicroseconds(10);
   digitalWrite(LEFT_T, LOW);
-  return float(pulseIn(LEFT_E, HIGH)) * SENSOR_CALC;
+  return float(pulseIn(LEFT_E, HIGH)) / SENSOR_CALC;
 }
 
 float checkFrontSensor() {
@@ -53,7 +62,7 @@ float checkFrontSensor() {
   digitalWrite(FRONT_T, HIGH);
   delayMicroseconds(10);
   digitalWrite(FRONT_T, LOW);
-  return float(pulseIn(FRONT_E, HIGH)) * SENSOR_CALC;
+  return float(pulseIn(FRONT_E, HIGH)) / SENSOR_CALC;
 }
 
 void goForwards() {
@@ -61,8 +70,8 @@ void goForwards() {
   digitalWrite(MOTOR_L_B, LOW);
   digitalWrite(MOTOR_R_F, HIGH);
   digitalWrite(MOTOR_R_B, LOW);
-  analogWrite(MOTOR_SPEED_L, 180);
-  analogWrite(MOTOR_SPEED_R, 180);
+  analogWrite(MOTOR_SPEED_L, SPEED);
+  analogWrite(MOTOR_SPEED_R, SPEED);
 }
 
 void goBackwards() {
@@ -70,8 +79,8 @@ void goBackwards() {
   digitalWrite(MOTOR_L_B, HIGH);
   digitalWrite(MOTOR_R_F, LOW);
   digitalWrite(MOTOR_R_B, HIGH);
-  analogWrite(MOTOR_SPEED_L, 180);
-  analogWrite(MOTOR_SPEED_R, 180);
+  analogWrite(MOTOR_SPEED_L, SPEED);
+  analogWrite(MOTOR_SPEED_R, SPEED);
 }
 
 void goLeft() {
@@ -79,8 +88,8 @@ void goLeft() {
   digitalWrite(MOTOR_L_B, LOW);
   digitalWrite(MOTOR_R_F, HIGH);
   digitalWrite(MOTOR_R_B, LOW);
-  analogWrite(MOTOR_SPEED_L, 100);
-  analogWrite(MOTOR_SPEED_R, 230);
+  analogWrite(MOTOR_SPEED_L, SPEED/2);
+  analogWrite(MOTOR_SPEED_R, SPEED*7/4);
 }
 
 void goRight() {
@@ -88,6 +97,15 @@ void goRight() {
   digitalWrite(MOTOR_L_B, LOW);
   digitalWrite(MOTOR_R_F, LOW);
   digitalWrite(MOTOR_R_B, HIGH);
-  analogWrite(MOTOR_SPEED_L, 180);
-  analogWrite(MOTOR_SPEED_R, 180);
+  analogWrite(MOTOR_SPEED_L, SPEED*3/2);
+  analogWrite(MOTOR_SPEED_R, SPEED*3/2);
+}
+
+void goSmallRight() {
+  digitalWrite(MOTOR_L_F, HIGH);
+  digitalWrite(MOTOR_L_B, LOW);
+  digitalWrite(MOTOR_R_F, HIGH);
+  digitalWrite(MOTOR_R_B, LOW);
+  analogWrite(MOTOR_SPEED_L, SPEED*5/4);
+  analogWrite(MOTOR_SPEED_R, SPEED*3/4);
 }
